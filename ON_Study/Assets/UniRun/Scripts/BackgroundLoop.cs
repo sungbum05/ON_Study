0cbfb25f4151af36d05b03129fa07136eba6c08f
Yongha <loha31707@gmail.com>
2022-07-21 23:10:20 +0900

    fix

--- ON_Study/Assets/UniRun/Scripts/BackgroundLoop.cs
+++ ON_Study/Assets/UniRun/Scripts/BackgroundLoop.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 
 // 왼쪽 끝으로 이동한 배경을 오른쪽 끝으로 재배치하는 스크립트
-public class BackgroundLoop : MonoBehaviour {
-    private float width; // 배경의 가로 길이
+public class BackgroundLoop : MonoBehaviour
+{
+    private float width; // 배경의 가로 길이 Spawner ssssss
 
-    private void Awake() {
+    private void Awake()
+    {
         // BoxCollider2D 컴포넌트의 Size 필드의 X 값을 가로 길이로 사용
         BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
         width = backgroundCollider.size.x;
     }
 
-    private void Update() {
+    private void Update()
+    {
         // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
         if (transform.position.x <= -width)
         {
@@ -19,9 +22,10 @@ public class BackgroundLoop : MonoBehaviour {
     }
 
     // 위치를 리셋하는 메서드
-    private void Reposition() {
+    private void Reposition()
+    {
         // 현재 위치에서 오른쪽으로 가로길이 * 2 만큼 이동
         Vector2 offset = new Vector2(width * 2f, 0);
-        transform.position = (Vector2) transform.position + offset;
+        transform.position = (Vector2)transform.position + offset;
     }
 }
